ad6edf5bbe2c0971a5792ca4be4300c2965c2d9d
Eugene Sentemov <eug.sent@gmail.com>
2022-03-26 21:30:45 +0500

    Third 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 	<h1>Test of revert</h1>
         <p>
-          Second 2
+          Third 3
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
