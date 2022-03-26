e507cb92fb5a28d3e42c08c301b6cbe2ef2c437b
Eugene Sentemov <eug.sent@gmail.com>
2022-03-26 21:30:25 +0500

    Second2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 	<h1>Test of revert</h1>
         <p>
-          First 1
+          Second 2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
